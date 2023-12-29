81ec7e680bb3cc50f6805d428368a0aa50de9224
Sherzod <fayziyev658@gmail.com>
2023-12-29 12:26:18 +0500

    RingView

---
+++ DesignCode1/RingView.swift
@@ -0,0 +1,18 @@
+//
+//  RingView.swift
+//  DesignCode1
+//
+//  Created by Sherzod Fayziev on 28/12/23.
+//
+
+import SwiftUI
+
+struct RingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RingView()
+}
