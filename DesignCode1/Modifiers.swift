3950fb98d0a6b275f076eb60cfb329c558bf620f
Sherzod <fayziyev658@gmail.com>
2024-01-04 22:43:31 +0500

    modifier

---
+++ DesignCode1/Modifiers.swift
@@ -0,0 +1,18 @@
+//
+//  Modifiers.swift
+//  DesignCode1
+//
+//  Created by Sherzod Fayziev on 04/01/24.
+//
+
+import SwiftUI
+
+struct Modifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Modifiers()
+}
